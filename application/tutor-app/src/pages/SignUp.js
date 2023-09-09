4de96cad4fcaae8f9748a84c2d82ab04469b0a5c
Michael Mathews <michael.mathews1999@gmail.com>
2023-09-09 00:02:50 -0700

    started signin/signup pages

--- application/tutor-app/src/pages/SignUp.js
+++ application/tutor-app/src/pages/SignUp.js
@@ -1,11 +1,81 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../index.css';
 
 function SignUp() {
   useEffect(() => {
     document.title = "Tutors.tech: Sign Up";
   }, []);
-  return <h1 className='header'>Sign Up</h1>;
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRememberMeChange = (e) => {
+    setRememberMe(e.target.checked);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Remember Me:", rememberMe);
+  };
+
+  return (
+    <div>
+      <h1 className='header'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='form-color'>
+        <div className="form-group">
+          <label htmlFor="email" className='no-select'>Email: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className='no-select'>Password: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="rememberMe" className='no-select'>
+            Remember me: 
+            <input
+              type="checkbox"
+              id="rememberMe"
+              name="rememberMe"
+              checked={rememberMe}
+              onChange={handleRememberMeChange}
+            />
+          </label>
+        </div>
+        <div className="center-button">
+          <button type="submit">Sign Up</button>
+        </div>
+        <div className="center-button">
+          <button type="submit">Forgot Password?</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
