ec70f6f9f4a1711a3794c52d2087432b11b317ee
Michael Mathews <michael.mathews1999@gmail.com>
2023-09-09 23:27:16 -0700

    added reading from bio text files & updated profile headers

--- application/tutor-app/src/App.js
+++ application/tutor-app/src/App.js
@@ -8,6 +8,13 @@ import SignUp from './pages/SignUp';
 import Home from './pages';
 import Profile from './pages/Profile';
 
+import MichaelMathewsBio from './data/Michael_Mathews.txt';
+import DanialTahirBio from './data/Danial_Tahir.txt';
+import AvaAlbertBio from './data/Ava_Albert.txt';
+import GriffinEvansBio from './data/Griffin_Evans.txt';
+import BryanMaldonadoBio from './data/Bryan_Maldonado.txt';
+import ClevelandPlonseyBio from './data/Cleveland_Plonsey.txt';
+
 function App() {
   return (
     <Router>
@@ -17,12 +24,12 @@ function App() {
         <Route path='/AboutUs' element={<AboutUs />} />
         <Route path='/SignIn' element={<SignIn />} />
         <Route path='/SignUp' element={<SignUp />} />
-        <Route path="/profiles/MichaelMathews" element={<Profile img="/images/favicon.png" name="Michael Mathews" bio="hi"/>} />
-        <Route path="/profiles/DanialTahir" element={<Profile img="/images/favicon.png" name="Danial Tahir" bio=""/>} />
-        <Route path="/profiles/AvaAlbert" element={<Profile img="/images/favicon.png" name="Ava Albert" bio=""/>} />
-        <Route path="/profiles/GriffinEvans" element={<Profile img="/images/favicon.png" name="Griffin Evans" bio=""/>} />
-        <Route path="/profiles/BryanMaldonado" element={<Profile img="/images/favicon.png" name="Bryan Maldonado" bio=""/>} />
-        <Route path="/profiles/ClevelandPlonsey" element={<Profile img="/images/favicon.png" name="Cleveland Plonsey" bio=""/>} />
+        <Route path="/profiles/MichaelMathews" element={<Profile img="/images/favicon.png" name="Michael Mathews" bio={MichaelMathewsBio}/>} />
+        <Route path="/profiles/DanialTahir" element={<Profile img="/images/favicon.png" name="Danial Tahir" bio={DanialTahirBio}/>} />
+        <Route path="/profiles/AvaAlbert" element={<Profile img="/images/favicon.png" name="Ava Albert" bio={AvaAlbertBio}/>} />
+        <Route path="/profiles/GriffinEvans" element={<Profile img="/images/favicon.png" name="Griffin Evans" bio={GriffinEvansBio}/>} />
+        <Route path="/profiles/BryanMaldonado" element={<Profile img="/images/favicon.png" name="Bryan Maldonado" bio={BryanMaldonadoBio}/>} />
+        <Route path="/profiles/ClevelandPlonsey" element={<Profile img="/images/favicon.png" name="Cleveland Plonsey" bio={ClevelandPlonseyBio}/>} />
       </Routes>
     </Router>
   );
--- application/tutor-app/src/pages/Profile.js
+++ application/tutor-app/src/pages/Profile.js
@@ -1,15 +1,26 @@
 import '../index.css';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Profile({img,name,bio}) {
+    const [fileContent, setFileContent] = useState('');
     useEffect(() => {
         document.title = name;
-      }, [name]);
+        fetch(bio)
+      .then((response) => response.text())
+      .then((data) => {
+        setFileContent(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching file:', error);
+      });
+      }, [name,bio]);
+
+      const updateName = name.endsWith('s') ? `${name}' Profile` : `${name}'s Profile`;
   return (
     <div>
-      <h1 className='header'>{name}'s Profile</h1>
-      <p className='bio'>{bio}</p>
-      <img src={img} alt={name}/>
+      <h1 className='header'>{updateName}</h1>
+      <div className='bio'><img src={img} alt={name} /></div>
+      <p className='bio'>{fileContent}</p>
     </div>
   );
 }
