173a933c08c1f8f342bad1e39a9e84d6fa5a7e4c
CleveyP <clevelandplonsey@gmail.com>
2023-09-14 04:16:24 -0700

    add todo and comments to authmiddleware.js

--- application/server/middleware/authMiddleware.js
+++ application/server/middleware/authMiddleware.js
@@ -4,18 +4,20 @@ const isLoggedIn = (req, res, next) => {
       next();
     }
     else{
-      //res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+      //TODO: if they are not logged in, should we reroute them to login page?
+      //res.sendFile(path.join(__dirname, '../tutor-app/build', 'index.html'));
       console.log("not authenticated " + JSON.stringify(req.session));
       res.send('Log in first to access this page.');
     }
   }
 
+//middleware that checks if user is the admin
   const isAdmin = (req, res, next) => {
     if(req.session.isAdmin){
       next();
     }
     else{
-      //res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+      //res.sendFile(path.join(__dirname, '../tutor-app/build', 'index.html'));
       console.log("not authenticated " + JSON.stringify(req.session));
       res.send('Must be logged in as admin.');
     }
