8897d375a09a3a03fa3aef5c3bbf2b9872007dab
Michael Mathews <michael.mathews1999@gmail.com>
2023-10-15 20:21:30 -0700

    update create post

--- application/tutor-app/src/components/userProfile/editPages/CreatePost.jsx
+++ application/tutor-app/src/components/userProfile/editPages/CreatePost.jsx
@@ -1,55 +1,98 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./editPage.scss";
-//import { cookie } from "../../../App";
 import axios from "axios";
 
 const CreatePost = () => {
-    const [postContent, setPostContent] = useState("");
+  const [postContent, setPostContent] = useState("");
+  const [pdfFile, setPdfFile] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
+  const [videoFile, setVideoFile] = useState(null);
 
-    //clears the text area
-    const handleClear = () => {
-        setPostContent("");
-    }
-  
-    const handlePost = async () => {
-      try {
-        const response = await axios.post(
-          `${process.env.REACT_APP_BACKEND_URL}/CreatePost`,
-          {
-            content: postContent,
+  const handleClear = () => {
+    setPostContent("");
+  };
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("content", postContent);
+    formData.append("pdfFile", pdfFile);
+    formData.append("imageFile", imageFile);
+    formData.append("videoFile", videoFile);
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/CreatePost`,
+        formData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
           },
-          {
-            withCredentials: true,
-          }
-        );
-  
-        if (response.data.success) {
-          setPostContent(""); 
-        } else {
-          console.error("Failed to create the post:", response.data.error);
         }
-      } catch (error) {
-        console.error("Error making the POST request:", error);
+      );
+
+      if (response.data.success) {
+        setPostContent("");
+        setPdfFile(null);
+        setImageFile(null);
+        setVideoFile(null);
+      } else {
+        console.error("Failed to create the post:", response.data.error);
       }
-    };
-  
-    return (
-      <div>
-        <h1 className="post-header">Create Post</h1>
-        <div className="post-area">
-            <textarea
-                className="post-textarea"
-                placeholder="Write your post here"
-                value={postContent}
-                onChange={(e) => setPostContent(e.target.value)}
-            />
+    } catch (error) {
+      console.error("Error making the POST request:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="post-header">Create Post</h1>
+      <div className="post-area">
+        <div className="post-textarea-container">
+          <textarea
+            className="post-textarea"
+            placeholder="Write your post here"
+            value={postContent}
+            onChange={(e) => setPostContent(e.target.value)}
+          />
         </div>
-        <div className="post-button-container">
-            <button className="create-button" onClick={handlePost}>Create Post</button>
-            <button className="create-button" onClick={handleClear}>Clear</button>
+        <div className="upload-container">
+          <div className="upload-input">
+            <span>Upload CV:</span>
+            <input
+              type="file"
+              accept=".pdf"
+              onChange={(e) => setPdfFile(e.target.files[0])}
+            />
+          </div>
+          <div className="upload-input">
+            <span>Upload Flier:</span>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setImageFile(e.target.files[0])}
+            />
+          </div>
+          <div className="upload-input">
+            <span>Upload Video:</span>
+            <input
+              type="file"
+              accept="video/*"
+              onChange={(e) => setVideoFile(e.target.files[0])}
+            />
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default CreatePost;
+      <div className="post-button-container">
+        <button className="create-button" onClick={handlePost}>
+          Create Post
+        </button>
+        <button className="create-button" onClick={handleClear}>
+          Clear
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
