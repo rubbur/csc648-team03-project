315c9173dbad82e67b954122d55a5e07fbcedf4e
Michael Mathews <michael.mathews1999@gmail.com>
2023-10-18 19:48:25 -0700

    add seeReviews

---
+++ application/tutor-app/src/components/userProfile/editPages/seeReviews.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ReviewCard from "../../tutorProfile/ReviewCard"; // Import the ReviewCard component
+import { cookie } from "../../../App";
+
+const SeeReviews = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        // Fetch reviews from the backend
+        async function fetchReviews() {
+            try {
+                const reviewResults = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/tutor/getTutorReviews`, { id: cookie.get("userId") }, { withCredentials: true });
+                if (!reviewResults.data.success) {
+                    console.log(reviewResults.data.errorMessage);
+                    return;
+                }
+                setReviews([...reviewResults.data.reviews]);
+                console.log(reviewResults.data);
+            } catch (error) {
+                console.error("Error fetching reviews:", error);
+            }
+        }
+
+        fetchReviews();
+    }, []);
+
+    return (
+        <div className="see-reviews edit-form">
+            <h2>{cookie.get("userName")}'s Reviews</h2>
+            {reviews.map((review, index) => (
+                <ReviewCard key={index} review={review} />
+            ))}
+        </div>
+    );
+};
+
+export default SeeReviews;
