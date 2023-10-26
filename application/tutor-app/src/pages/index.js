5957d19920c3ce9809318e9fa1b23718739da345
Michael Mathews <michael.mathews1999@gmail.com>
2023-10-26 13:02:39 -0700

    remove photo from index / home page

--- application/tutor-app/src/pages/index.js
+++ application/tutor-app/src/pages/index.js
@@ -12,20 +12,21 @@ const Home = () => {
             const scrollY = window.scrollY || window.pageYOffset;
             const backgroundContainer = backgroundContainerRef.current;
             if (backgroundContainer) {
-                const newPosition = `${-scrollY*0.5}px -280px`; 
-                backgroundContainer.style.backgroundPosition = newPosition; 
+                const newPosition = `${-scrollY * 0.5}px -280px`;
+                backgroundContainer.style.backgroundPosition = newPosition;
             }
         };
 
-        window.addEventListener('scroll', updateBackgroundPosition); 
+        window.addEventListener('scroll', updateBackgroundPosition);
         return () => {
-            window.removeEventListener('scroll', updateBackgroundPosition); 
+            window.removeEventListener('scroll', updateBackgroundPosition);
         };
     }, []);
 
     return (
         <div className='long-page'>
-            <div className='background-container' ref={backgroundContainerRef}></div>
+            {/* here lies my moving image */}
+            {/*<div className='background-container' ref={backgroundContainerRef}></div>*/}
             <h1 className='pageHeader'>Welcome to Tutors.tech at SFSU</h1>
         </div>
     );
